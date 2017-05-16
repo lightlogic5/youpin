623562b97bc42698f00dc7d6dfc9af7047fddcf5
guiyuan <email@example.com>
2017-05-16 10:53:05 +0800

    在apps.py中修改app的中文显示

--- apps/institutions/apps.py
+++ apps/institutions/apps.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
@@ -5,3 +6,5 @@ from django.apps import AppConfig
 
 class InstitutionsConfig(AppConfig):
     name = 'institutions'
+    verbose_name = u"考核制度管理"
+
